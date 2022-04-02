1f91a070d3a77a8be0d28d1e1283ce9039bdb431
Extaz4you <nikitqalevioffanov@gmaol.com>
2022-04-02 15:42:27 +0300

    Change 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.Clear(); // Очищенте консоли
-string[] Word = { "Age", "Place", "Pen", "Class", "Work", "Book" };
-var SelectWord = from s in Word where(s.Length <=3) select s;
-foreach (string text in SelectWord)
-Console.WriteLine(text);
+string[] result = (from arg in args             // решение задачи
+                   where arg.Length <= 3
+                   select arg).ToArray();
+Console.Write(string.Join(",",result));
+
+
