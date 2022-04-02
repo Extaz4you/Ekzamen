ca7a36d213d21742d54aef1b0908f1d351b8a3eb
Extaz4you <nikitqalevioffanov@gmaol.com>
2022-04-02 15:20:09 +0300

    Change

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.Clear(); // Очищенте консоли
+string[] Word = { "Age", "Place", "Pen", "Class", "Work", "Book" };
+var SelectWord = from s in Word where(s.Length <=3) select s;
+foreach (string text in SelectWord)
+Console.WriteLine(text);
